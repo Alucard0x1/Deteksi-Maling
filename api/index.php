4a31840483d1e3bed1ebbe3e7ec9dfeee7adf6a0
Alucard0x1 <lombokweb@gmail.com>
2024-09-26 22:25:28 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -12,7 +12,7 @@ header('Content-Type: text/html; charset=UTF-8');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deteksi Maling Berdasarkan Primbon Jawa</title>
+    <title>Deteksi Maling Berdasarkan Neptu Jawa</title>
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <!-- Custom CSS -->
@@ -27,7 +27,7 @@ header('Content-Type: text/html; charset=UTF-8');
                 <input type="date" id="tanggal_lahir" name="tanggal_lahir" required aria-describedby="tanggalLahirHelp" 
                        oninvalid="this.setCustomValidity('Silakan pilih tanggal lahir korban')" 
                        oninput="this.setCustomValidity('')">
-                <div id="tanggalLahirHelp" class="form-text">Pilih tanggal lahir korban.</div>
+
             </div>
 
             <div class="form-group">
@@ -35,13 +35,13 @@ header('Content-Type: text/html; charset=UTF-8');
                 <input type="date" id="tanggal_kehilangan" name="tanggal_kehilangan" required aria-describedby="tanggalKehilanganHelp" 
                        oninvalid="this.setCustomValidity('Silakan pilih tanggal kehilangan')" 
                        oninput="this.setCustomValidity('')">
-                <div id="tanggalKehilanganHelp" class="form-text">Pilih tanggal kehilangan.</div>
+
             </div>
 
-            <button type="submit">Hitung Pelaku</button>
+            <button type="submit">Hitung Neptu untuk Deteksi Pelaku</button>
 
             <!-- Loading Spinner -->
-            <div id="loading" class="loading hidden">Loading...</div>
+            <div id="loading" class="loading hidden">🦹‍♂️</div>
         </form>
 
         <?php
