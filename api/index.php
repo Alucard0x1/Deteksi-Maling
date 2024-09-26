64809fab659664e97511c88c9f51b545dd6a2d92
Alucard0x1 <lombokweb@gmail.com>
2024-09-26 21:35:26 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -13,7 +13,7 @@ header('Content-Type: text/html; charset=UTF-8');
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <!-- CSS Styles -->
-    <link rel="stylesheet" href="/public/style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
     <div class="container">
